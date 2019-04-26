5f4a391dbceb16caa00c71f7b9dddd9396c2c24d
Md. Morshadun Nur <morshadunnur@gmail.com>
2019-04-26 15:40:10 +0600

    Qroles Setup

---
+++ packages/Qbytesoft/qroles/src/QrolesServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+namespace Qbytesoft\Qroles;
+
+use Illuminate\Support\ServiceProvider;
+
+class QrolesServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
+
