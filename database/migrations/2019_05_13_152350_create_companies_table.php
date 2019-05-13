39b36bf1a8fe068cb8ff55d007ccc5a9b0d156ae
Imtiaze <ahammedimtiaze78@gmail.com>
2019-05-13 22:01:04 +0600

    Initial migration

---
+++ database/migrations/2019_05_13_152350_create_companies_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->string('representative_name');
+            $table->string('phone1');
+            $table->string('phone2');
+            $table->string('email')->unique();
+            $table->text('address');
+            $table->decimal('opening_balance', 15,2)->default(0);
+            $table->dateTime('starting_date');
+            $table->dateTime('ending_date');
+            $table->enum('status', ['active', 'inactive', 'disabled']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- BEGIN HEAD -->
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta name="description" content="Responsive Admin Template" />
+    <meta name="author" content="SmartUniversity" />
+    <title>@yield('title')</title>
+    <!-- google font -->
+   
+   
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet" type="text/css" />
+	<!-- icons -->
+    <link href="{{ asset('admin/assets/plugins/simple-line-icons/simple-line-icons.min.css') }} " rel="stylesheet" type="text/css" />
+    <link href="{{ asset('admin/fonts/font-awesome/css/font-awesome.min.css') }} " rel="stylesheet" type="text/css"/>
+	<link href="{{ asset('admin/fonts/material-design-icons/material-icon.css') }} " rel="stylesheet" type="text/css" />
+	<!--bootstrap -->
+	<link href="{{ asset('admin/assets/plugins/bootstrap/css/bootstrap.min.css') }} " rel="stylesheet" type="text/css" />
+	<link href="{{ asset('admin/assets/plugins/summernote/summernote.css') }} " rel="stylesheet">
+    <!-- Material Design Lite CSS -->
+	<link rel="stylesheet" href="{{ asset('admin/assets/plugins/material/material.min.css') }} ">
+	<link rel="stylesheet" href="{{ asset('admin/assets/css/material_style.css') }} ">
+	<!-- animation -->
+	<link href="{{ asset('admin/assets/css/pages/animate_page.css') }} " rel="stylesheet">
+	<!-- inbox style -->
+    <link href="{{ asset('admin/assets/css/pages/inbox.min.css') }} " rel="stylesheet" type="text/css" />
+	<!-- Theme Styles -->
+    
+    <link href="{{ asset('admin/assets/css/plugins.min.css') }} " rel="stylesheet" type="text/css" />
+   
+    <link href="{{ asset('admin/assets/css/theme-color.css') }} " rel="stylesheet" type="text/css" />
+	<!-- favicon -->
+    <link rel="shortcut icon" href="{{ asset('admin/assets/img/favicon.ico') }} " /> 
+
+    <link href="{{ asset('admin/assets/css/style.css') }} " rel="stylesheet" type="text/css" />
+    <link href="{{ asset('admin/assets/css/responsive.css') }} " rel="stylesheet" type="text/css" />
+ 
+
+</head>
+ <!-- END HEAD -->
+<body class="page-header-fixed sidemenu-closed-hidelogo page-content-white page-md header-white dark-sidebar-color logo-dark">
+    <div class="page-wrapper">
+        <!-- start header -->
+        @include('layouts.partials.topbar')
+        <!-- end header -->
+
+
+        <!-- start page container -->
+        <div class="page-container">
+
+            <!-- start sidebar menu -->
+ 			@include('layouts.partials.sidebar')
+            <!-- end sidebar menu -->
+            
+            <!-- start page content -->
+            <div class="page-content-wrapper">
+                <div class="page-content">
+                    @yield('content')
+                </div>
+            </div>
+            <!-- end page content -->
+
+        </div>
+        <!-- end page container -->
+
+
+
+        <!-- start footer -->
+        {{-- @include('layouts.partials.footer') --}}
+        <!-- end footer -->
+
+
+    </div>
+
+
+ <!-- start js include path -->
+ <script src="{{ asset('admin/assets/plugins/jquery/jquery.min.js') }} " ></script>
+ <script src="{{ asset('admin/assets/plugins/popper/popper.min.js') }} " ></script>
+ <script src="{{ asset('admin/assets/plugins/jquery-blockui/jquery.blockui.min.js') }} " ></script>
+ <script src="{{ asset('admin/assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js') }} "></script>
+ <!-- bootstrap -->
+ <script src="{{ asset('admin/assets/plugins/bootstrap/js/bootstrap.min.js') }} " ></script>
+ <script src="{{ asset('admin/assets/plugins/sparkline/jquery.sparkline.min.js') }} " ></script>
+ <script src="{{ asset('admin/assets/js/pages/sparkline/sparkline-data.js') }} " ></script>
+ <!-- Common js-->
+ <script src="{{ asset('admin/assets/js/app.js') }} " ></script>
+ <script src="{{ asset('admin/assets/js/layout.js') }} " ></script>
+ <script src="{{ asset('admin/assets/js/theme-color.js') }} " ></script>
+ <!-- material -->
+ <script src="{{ asset('admin/assets/plugins/material/material.min.js') }} "></script>
+ <!-- animation -->
+ <script src="{{ asset('admin/assets/js/pages/ui/animations.js') }} " ></script>
+ <!-- chart js -->
+ <script src="{{ asset('admin/assets/plugins/chart-js/Chart.bundle.js') }} " ></script>
+ <script src="{{ asset('admin/assets/plugins/chart-js/utils.js') }} " ></script>
+ <script src="{{ asset('admin/assets/js/pages/chart/chartjs/home-data.js')}} " ></script>
+ <!-- summernote -->
+ <script src="{{ asset('admin/assets/plugins/summernote/summernote.min.js') }} " ></script>
+ <script src="{{ asset('admin/assets/js/pages/summernote/summernote-data.js') }} " ></script>
+ <!-- end js include path -->
+</body>
+</html>
